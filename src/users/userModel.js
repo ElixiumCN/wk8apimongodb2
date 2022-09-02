83b941497e3a0206160fcee874949391778e96d1
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-09-02 12:33:39 +0100

    Update userModel.js

--- src/users/userModel.js
+++ src/users/userModel.js
@@ -4,12 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
     {
-        username: 
-        {
-            type: String,
-            required: true,
-            unique: true
-        },
         email:
         {
             type: String,
